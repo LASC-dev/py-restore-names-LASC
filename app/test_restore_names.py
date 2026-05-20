5ecbb45b54d1343f6772f8e21adee418597e915e
LASC-dev <lucas.a.s.casas@outlook.es>
2026-05-19 21:57:10 -0300

    test(restore_names): cover missing first_name cases

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,58 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_with_not_first_name() -> None:
+    users = [
+        {
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    result = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == result
+
+
+def test_with_none_as_first_name() -> None:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": None,
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    result = [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+    restore_names(users)
+    assert users == result
